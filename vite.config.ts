5815869a2abdd765521b8225e0b44fb2a47315d4
meuphilim <meuphilim@gmail.com>
2025-05-27 09:29:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/pousadadoinacio.github.io/", // GitHub Pages na raiz do domínio
+  base: '/',
   server: {
     host: "::",
     port: 8080,
